7bde71b3861027dcf36903e81ac4406fa4f268f5
Abel You <abelyou032@gmail.com>
2025-06-15 11:54:22 +0800

    Refactor painting algorithm for efficiency and clarity

--- toonz/sources/tnztools/paintbrushtool.cpp
+++ toonz/sources/tnztools/paintbrushtool.cpp
@@ -340,7 +340,7 @@ PaintBrushTool::PaintBrushTool()
     , m_workingFrameId(TFrameId())
     , m_modifierLockAlpha("Lock Alpha", false)
     , m_pick("Pick", false) {
-  // m_toolSize.setNonLinearSlider();
+  m_toolSize.setNonLinearSlider();
 
   m_colorType.addValue(LINES);
   m_colorType.addValue(AREAS);
